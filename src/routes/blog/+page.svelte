b7372078640e1e1dd32284feb80130ad22c7d701
Lukas Schneider <lukasschneider@LaptopvonLukas.fritz.box>
2024-05-13 17:50:13 +0200

    ADD blog site

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    const data = {
+        title: "Test Blog",
+        imageUrl: "https://picsum.photos/1200/800?random=5",
+        author: "Lukas Schneider",
+        lead: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur mollitia fuga reiciendis cupiditate magni illo ad expedita? Quas, nostrum nemo adipisci sit earum, libero quod nisi velit necessitatibus ducimus accusantium ab molestiae explicabo saepe accusamus minima neque eius hic consequatur sunt ad quia blanditiis aliquid. Accusantium assumenda veritatis tempore aspernatur?",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cum repellat sed. Dolore, excepturi nihil? Error mollitia libero facilis neque eius perferendis enim nihil commodi temporibus aliquid. Mollitia iusto hic adipisci aspernatur nesciunt deserunt accusamus? Quam nobis minus quae totam eligendi aliquid vitae ratione vel numquam? Tenetur reiciendis dignissimos, dolor quisquam adipisci quo aliquid est neque nostrum perspiciatis officiis, minima excepturi numquam modi exercitationem quia, hic itaque? Sequi nostrum, vero aliquam voluptatibus est vitae ex atque totam consequuntur reprehenderit aliquid velit rem reiciendis tempore cum eum exercitationem recusandae neque sunt saepe! Deserunt libero quo dolor! Aperiam repellendus doloribus ad animi quaerat officia accusamus corrupti iure voluptatibus error porro, maiores sunt reprehenderit numquam similique nihil rerum quidem tenetur quisquam enim labore deserunt tempora. Minus, dolorum. Animi nobis incidunt distinctio, ipsam, vero reiciendis recusandae itaque eius doloremque commodi odio ullam molestiae? A illum ea consequuntur reiciendis, nulla vitae exercitationem eum ipsa quos sunt porro, deleniti eveniet! Error voluptates sit possimus. Corporis ipsa ducimus corrupti ad. Quibusdam doloremque fugit laudantium pariatur perferendis minus recusandae modi rerum aut accusamus autem quia mollitia, nemo itaque excepturi enim asperiores dolores suscipit harum ab odio aspernatur magni rem voluptatibus. Neque vel accusantium consectetur cum molestiae sit, esse adipisci. Esse, consequuntur repudiandae! Iste rem odit facilis quod id fuga magni voluptatum? At obcaecati harum ut incidunt quibusdam eos explicabo officia totam magnam, deleniti esse corrupti! Saepe totam sapiente inventore mollitia magnam illo amet maiores corporis minima nobis ea enim, aliquid nam ipsam tempore consectetur placeat veritatis distinctio eligendi reprehenderit? Nesciunt quia dicta repellendus modi ullam quos consequatur? Excepturi fuga architecto placeat illum eum beatae nemo natus id rerum repellat. Iure culpa, temporibus quisquam, quidem sapiente labore aut quasi illo esse cupiditate at mollitia voluptatem aliquam quaerat harum minima libero architecto nam laboriosam sed provident facere! Consequuntur, qui repudiandae?"
+    }
+</script>
+
+<div class="w-screen h-full py-24 px-36 flex flex-col">
+    <header class="w-screen h-full py-24 px-36 flex flex-col">
+        <div class="w-[40%] h-1/4">
+            <img class="h-full rounded-xl" src={data.imageUrl} alt="">
+        </div>
+        <h1 >
+            {data.title}
+        </h1>
+    </header>
+    <main>s</main>
+    <footer>s</footer>
+</div>
+
